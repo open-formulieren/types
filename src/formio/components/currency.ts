2be03b0850148c5276093fa50852f8f263c0323c
Viicos <65306057+Viicos@users.noreply.github.com>
2023-11-10 15:30:11 +0100

    🐛 `currency` is not multiple capable

--- src/formio/components/currency.ts
+++ src/formio/components/currency.ts
@@ -1,4 +1,4 @@
-import {InputComponentSchema, MultipleCapable} from '..';
+import {InputComponentSchema} from '..';
 
 type Validator = 'required' | 'min' | 'max';
 type TranslatableKeys = 'label' | 'description' | 'tooltip';
@@ -7,18 +7,13 @@ export type CurrencyInputSchema = InputComponentSchema<number, Validator, Transl
 
 /**
  * @group Form.io components
- * @category Base types
+ * @category Concrete types
  */
-export interface BaseCurrencyComponentSchema extends Omit<CurrencyInputSchema, 'hideLabel'> {
+export interface CurrencyComponentSchema extends Omit<CurrencyInputSchema, 'hideLabel'> {
   type: 'currency';
   // additional properties
   currency: 'EUR';
   decimalLimit?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
   allowNegative?: boolean;
+  defaultValue?: number;
 }
-
-/**
- * @group Form.io components
- * @category Concrete types
- */
-export type CurrencyComponentSchema = MultipleCapable<BaseCurrencyComponentSchema>;
