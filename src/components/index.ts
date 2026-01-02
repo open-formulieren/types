6fd3489fec85811da566b838988667fb24fd1bfd
Sergei Maertens <sergei@maykinmedia.nl>
2026-01-02 12:38:10 +0100

    :recycle: [#81] Move the FallbackSchema

--- src/components/index.ts
+++ src/components/index.ts
@@ -119,3 +119,14 @@ export {
   // deprecated
   CosignV1ComponentSchema,
 };
+
+/**
+ * A fallback, minimal schema to handle the cases where component.type is something unknown.
+ *
+ * This could be because someone pasted a JSON configuration from Form.io's hosted
+ * form builder into our components or just wrote component definitions directly to our
+ * API endpoints.
+ */
+export interface FallbackSchema {
+  type?: string;
+}
